e16b420db597b9629474d0f6636e9eaf94656597
Dianliang233 <dianliang233@gmail.com>
2022-05-05 09:29:58 +0800

    Action plz

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import components from 'unplugin-vue-components/vite'
 import { Vuetify3Resolver } from 'unplugin-vue-components/resolvers'
 import visualizer from 'rollup-plugin-visualizer'
-
-const path = require('path')
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
